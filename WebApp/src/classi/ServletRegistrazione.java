6d41622725020790d9b60e2fa41161d5713d0b09
Alessio-01 <75307110+Alessio-01@users.noreply.github.com>
2021-04-20 21:38:16 +0200

    Add files via upload
    creata dal jacobs

---
+++ WebApp/src/classi/ServletRegistrazione.java
@@ -0,0 +1,93 @@
+
+package classi;
+import JavaBeans.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletRegistrazione
+ */
+@WebServlet("/ServletRegistrazione")
+public class ServletRegistrazione extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletRegistrazione() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		// Altro codice a mia discrezione
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+		// Altro a mia discrezione
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String Username = request.getParameter("username");
+		String Password = request.getParameter("password");
+		String Email = request.getParameter("email");
+		Date DataNascita = request.getParameter("dataNascita");
+		
+		// Esempio
+		response.setContentType("text/html");
+
+		ServletContext sc = request.getSession().getServletContext();
+		request.removeAttribute("Utenti");
+		DBManagement listUtenti = new DBManagement();
+		try
+		{
+			utenti = listUtenti.Registrazione(Username,Password,Email,DataNascita);
+				
+				request.getSession().setAttribute("Utente", Username);
+				RequestDispatcher rd = sc.getRequestDispatcher("/presentazione.jsp");
+				rd.forward(request, response);
+			
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			RequestDispatcher rd = sc.getRequestDispatcher("/index.jsp");
+			rd.forward(request, response);
+		}
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	}
+
+}
